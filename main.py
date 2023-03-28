9ab5a920c0ae43335ead34b4cab3a1a8cfea2dbd
taymor <112526337+Taymorhugo@users.noreply.github.com>
2023-03-28 22:57:23 +0000

    aula da faculdade calculadora que soma, subtrai e multiflica

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def ligar():
 def deligar():
   power = "desligado"
   print(power)
-#desligar(
+#desligar()
 
 def somar(num1,num2):
   total = num1 + num2
   print(total)
-  somar(2,3)
+#somar(2,3)
 
 def menu ():
   print("--------Calculadora----------")
@@ -22,12 +22,41 @@ def menu ():
   print("1 - somar\n2 - subtrair\n3 - multiplicar")
   print("------------------------")
 
-  while True:
+def subtrair(num1,num2):
+  total = num1 - num2
+  print(total)
+
+def multiplicar(num1,num2):
+  total = num1 * num2
+  print(total)
+
+while True:
+  menu()
+  pegar = int(input("Escolha qual operação que deseja realizar: "))
+  if pegar == 1:
+    a = int(input("Insira o primeiro número: "))
+    b = int(input("Insira o segundo número: "))
+    somar(a,b)
     menu()
-    pegar = input("Escolha qual operação deseja realizar")
-    if pegar == 1:
-      a = int(input("Insira o primeiro número"))
-      b = int(input("Insira o segundo número"))
-      somar(a,b)
-      break
+  elif pegar == 2:
+    c = int(input("Insira o primeiro número: "))
+    d = int(input("Insira o segundo número: "))
+    subtrair(c,d)
+    menu()
+  elif pegar == 3:
+    e = int(input("Insira o primeiro número: "))
+    f = int(input("Insira o segundo número: "))
+    multiplicar(e,f)
+    break
+  
+
+
+  
+    
+    
+    
+  
+
+
+    
   
